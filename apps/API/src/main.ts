d76534c06ba4098e28b1bada93536f3f0fbffa62
FinarixEU <finario@outlook.com>
2025-10-28 19:46:16 +0100

    Update main.ts

--- apps/API/src/main.ts
+++ apps/API/src/main.ts
@@ -30,7 +30,6 @@ async function bootstrap() {
   const port = Number(process.env.PORT) || 4000;
   await app.listen(port, '0.0.0.0');
 
-  // eslint-disable-next-line no-console
   console.log(Finarix API läuft auf Port ${port});
 }
 
