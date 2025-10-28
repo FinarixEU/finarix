0e6de753c6955e1930282dc7a3044b90d1377855
FinarixEU <finario@outlook.com>
2025-10-28 13:43:10 +0100

    Create app.module.ts

---
+++ apps/API/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { VersionController } from './version.controller';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { FinanceModule } from './finance/finance.module';
+
+@Module({
+  imports: [PrismaModule, AuthModule, FinanceModule],
+  controllers: [AppController, VersionController],
+  providers: []
+})
+export class AppModule {}
