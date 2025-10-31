aee94ccd93aa3c5d04f1e45b839876fd5b6ef62e
FinarixEU <finario@outlook.com>
2025-10-31 12:50:47 +0100

    Update register.dto.ts

--- apps/API/src/auth/dto/register.dto.ts
+++ apps/API/src/auth/dto/register.dto.ts
@@ -1,12 +1,12 @@
-import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsString()
   @MinLength(6)
-  password: string;
+  password!: string;
 
   @IsOptional()
   @IsString()
